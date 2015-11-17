6f77062d7bf3671864ed9bfa5af85506d48f15f3
Belinschi Dima <belinski.d@gmail.com>
2015-11-17 15:57:46 +0200

    add migrations

--- app.rb
+++ app.rb
@@ -9,6 +9,9 @@ set :database, "sqlite3:barbershop.db"
 class Clients < ActiveRecord::Base 
 end
 
+class Barbers < ActiveRecord::Base 
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
