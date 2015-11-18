4b12fd18d7d909369e994220eff279ffcdd62cf0
Belinschi Dima <belinski.d@gmail.com>
2015-11-18 16:22:15 +0200

    Add validation

--- app.rb
+++ app.rb
@@ -7,7 +7,13 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:barbershop.db"
 
 class Client < ActiveRecord::Base 
+	validates :name, presence: true
+	validates :phone, presence: true
+	validates :barber, presence: true
+	validates :datestamp, presence: true
+	validates :color, presence: true
 end
+
 
 class Barber < ActiveRecord::Base 
 end
@@ -27,6 +33,7 @@ end
 post '/visit' do
 	c = Client.new params[:client]
 	c.save
+	erb :visit
 end
 
 
